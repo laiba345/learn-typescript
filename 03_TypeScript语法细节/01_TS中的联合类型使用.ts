11adbba4508f9ba5d3dc611049244f7122e285b2
Kang Huang <kangder@KangdeMacBook-Pro.local>
2023-03-14 10:30:36 +0800

    代码全实现

--- 03_TypeScript语法细节/01_TS中的联合类型使用.ts
+++ 03_TypeScript语法细节/01_TS中的联合类型使用.ts
@@ -1,23 +1,26 @@
-let foo: number | string = 'abc'
+// 1.联合类型的基本使用
+// let foo: number | string = "abc"
+// foo = 123
 
-foo = 'abc'
+// // 使用的时候要特别的小心
+// if (typeof foo === "string") {
+//   console.log(foo.length)
+// }
 
-// 使用的时候需要特别小心
-if (typeof foo === 'string') {
-    console.log(foo.length)
+
+// 2.举个栗子: 打印id
+function printID(id: number | string) {
+  console.log("您的ID:", id)
+
+  // 类型缩小
+  if (typeof id === "string") {
+    console.log(id.length)
+  } else {
+    console.log(id)
+  }
 }
 
-// 打印id
-function printId(id: number | string) {
-    console.log('您的ID:', id)
+printID("abc")
+printID(123)
 
-    // 类型缩小
-    if (typeof id === 'string') {
-        console.log(id.length)
-    } else {
-        console.log(id)
-    }
-}
-printId('abc')
-
-export {  } 
+export {  }
